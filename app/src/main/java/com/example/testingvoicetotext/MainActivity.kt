dcfca0d642c7d50beebeb837e0724807a2864bd6
kellyyyang <kelyang@hmc.edu>
2023-09-04 17:05:31 -0700

    demo

--- app/src/main/java/com/example/testingvoicetotext/MainActivity.kt
+++ app/src/main/java/com/example/testingvoicetotext/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.testingvoicetotext
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +15,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.testingvoicetotext.ui.theme.TestingVoicetoTextTheme
+import org.intellij.lang.annotations.Language
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+    private lateinit var textToSpeech:TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TestingVoicetoTextTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+        // text to speech button
+        val btnText = findViewById<Button>(R.id.btnText)
+
+        // text
+        val editText = findViewById<EditText>(R.id.Text)
+
+        // create TextToSpeech object
+        textToSpeech = TextToSpeech(this){status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val result = textToSpeech.setLanguage(Locale.getDefault())
+                if (result == TextToSpeech.LANG_MISSING_DATA) {
+                    Toast.makeText(this, "language not supported", Toast.LENGTH_LONG).show()
                 }
             }
+        }
+        // Setting On Click Listener
+        btnText.setOnClickListener {
+            if (editText.text.toString().trim().isNotEmpty()) {
+                textToSpeech.speak(editText.text.toString().trim(), TextToSpeech.QUEUE_FLUSH, null, null)
+            } else (
+                    Toast.makeText(this, "Required", Toast.LENGTH_SHORT).show()
+            )
         }
     }
 }
